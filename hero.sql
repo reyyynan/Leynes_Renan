c74de9286741c4139c57201de901cdad233610df
Renan <renanleynes@gmail.com>
2023-10-29 19:46:56 +0800

    feat/select-avg-playerlevel-per-class syntax

--- hero.sql
+++ hero.sql
@@ -168,3 +168,10 @@ SELECT hero.hero_name
 FROM public.hero
 JOIN public.class ON hero.class_id = class.class_id
 WHERE class.class_name LIKE '%Archers';
+
+SELECT c.class_name, AVG(p.player_level) AS avg_level
+FROM public.class c
+LEFT JOIN public.hero h ON c.class_id = h.class_id
+LEFT JOIN public.player p ON h.hero_id = p.hero_id
+GROUP BY c.class_name
+ORDER BY avg_level DESC;
